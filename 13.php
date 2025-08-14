8ca21e92ecb38d4cf3c3a46f8dda801304ff56f2
Aboda Owais <bodyahmedewees@gmail.com>
2025-08-14 07:43:28 +0300

    first commit

---
+++ 13.php
@@ -0,0 +1,24 @@
+<?php
+$fristname = ["fristname"];
+$lastname =  ["lastname"];
+$email= ["email"]
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>project my self</title>
+</head>
+<body>
+    <form action="13.php" method="get"></form>
+    <input type="text" name="firstname" id="firstname" placeholder="fname" requiard>
+    <input type="text" name="lastename" id="lastename" placeholder="lname" requiard>
+    <input type="text" name="email" id="email" placeholder="a@mail.com">
+    <input type="submit" value="send action">
+</body>
+</html>
